c0b5a63447626279a9fe3391de21d105c34f2211
Ardiansyah Sulistyo <ardiansyahdian755@gmail.com>
2025-02-27 18:12:36 +0700

    1. Pengenalan React, Komponen, & Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Welcome from "./components/Welcome";
+import HeaderCustom from "./components/HeaderCustom";
+import ProfileCard from "./components/ProfileCard";
 
+type Teacher = {
+  name: string;
+  year: number;
+  job: string;
+  id: number;
+};
+
+const teachers: Teacher[] = [
+  {
+    name: "Ardiansyah Sulistyo",
+    year: 2006,
+    job: "Student",
+    id: 1,
+  },
+  {
+    name: "Budi",
+    year: 2000,
+    job: "Content Creator",
+    id: 2,
+  },
+  {
+    name: "Usman",
+    year: 2002,
+    job: "Business Man",
+    id: 3,
+  },
+];
+
+// Component
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <HeaderCustom />
+      <h1>Hello World</h1>
+      <div style={{ display: "flex", gap: "8px", flexDirection: "row" }}>
+        {teachers.map((teacher) => {
+          return (
+            <ProfileCard
+              name={teacher.name}
+              year={teacher.year}
+              job={teacher.job}
+              key={teacher.id}
+            />
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Welcome />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
